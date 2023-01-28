3ee86d1277725cb2b81496bc1ff32721b3bd3925
Leiber Bertel <leiberbertel777@gmail.com>
2023-01-28 02:02:05 -0500

    Agregando archivo js terminado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,25 @@
 /*=============== SHOW HIDDEN - PASSWORD ===============*/
+const showHiddenPassword = (inputPass, inputIcon) =>{
+    const input = document.getElementById(inputPass),
+        iconEye = document.getElementById(inputIcon)
+
+    iconEye.addEventListener('click', () =>{
+        if(input.type === 'password'){
+            // Cambia a texto
+            input.type = 'text'
+            // Add icon
+            iconEye.classList.add('ri-eye-line')
+            // Remove icon
+            iconEye.classList.remove('ri-eye-off-line')
+        }else{
+            // Change to password
+            input.type = 'password'
+            // Remove icon
+            iconEye.classList.remove('ri-eye-line')
+            //Add icon
+            iconEye.classList.add('ri-eye-off-line')
+        }
+    })
+}
+
+showHiddenPassword('input-pass', 'input-icon')
